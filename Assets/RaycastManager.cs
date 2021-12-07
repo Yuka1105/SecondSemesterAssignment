8d8e3e75dbd9e6c513d6e1ed498818e717b845ca
Yuka1105 <MpY1105@gmail.com>
2021-12-07 19:44:17 +0900

    RaycastManagerを消さないオブジェクトに変えた

--- Assets/RaycastManager.cs
+++ Assets/RaycastManager.cs
@@ -22,6 +22,28 @@ public class SaveData //セーブデータ用のクラス
 }
 public class RaycastManager : MonoBehaviour
 {
+    private static RaycastManager instance = null;
+
+    // GameControllerインスタンスのプロパティーは、実体が存在しないとき（＝初回参照時）実体を探して登録する
+    public static RaycastManager Instance => instance
+        ?? ( instance = GameObject.FindWithTag ( "RaycastManager" ).GetComponent<RaycastManager> () );
+    void Awake()
+    {
+        // もしインスタンスが複数存在するなら、自らを破棄する
+        if ( this != Instance )
+        {
+            Destroy ( this.gameObject );
+            return;
+        }
+
+        // 唯一のインスタンスなら、シーン遷移しても残す
+        DontDestroyOnLoad ( this.gameObject );
+    }
+    private void OnDestroy ()
+    {
+        // 破棄時に、登録した実体の解除を行う
+        if ( this == Instance ) instance = null;
+    }
     GameObject FoodProvider;
     FoodProvider script;
     //DateTimeを使うため変数を設定
