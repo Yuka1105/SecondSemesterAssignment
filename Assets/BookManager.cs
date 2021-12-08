9d9552b3a3353a4eaaf3b701d16a50375500345f
Yuka1105 <MpY1105@gmail.com>
2021-12-08 16:22:41 +0900

    図鑑に入力回数、最後に買った日、ボタンなどの機能

--- Assets/BookManager.cs
+++ Assets/BookManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [System.Serializable]
 public class Item{//食材名、入力回数、最後に買った日付を格納するクラス
@@ -12,13 +13,22 @@ public class Item{//食材名、入力回数、最後に買った日付を格納
 
 public class BookManager : MonoBehaviour
 {
+    bool book_open;//図鑑のシーンに来た時1回だけListの中のデータを見る。
     GameObject RaycastManager;
     RaycastManager script;
-    Item[] item;
+    Item[] item = new Item[39];
+    public GameObject button;//アイテムのボタン
+    public GameObject content;//ボタンの親となるコンテント(UI)
+    public Text text_times;
+    public Text text_lastday;
+    public Text foodnametext;
     // Start is called before the first frame update
     void Start()
     {
-     item = new Item[39];//食材の種類分だけItemインスタンスを作る。
+     book_open = true;   
+     for(int i = 0; i < 39; i++){
+         item[i] = new Item();
+     }
      item[0].food = "米";
      item[1].food = "パン";
      item[2].food = "麺";
@@ -58,11 +68,7 @@ public class BookManager : MonoBehaviour
      item[36].food = "チョコレート";
      item[37].food = "クッキー";
      item[38].food = "ケーキ";
-     for(int i =0; i<39; i++){
-         item[i].times = 0;
-         item[i].month = 0;
-         item[i].day = 0;
-     }
+
      RaycastManager = GameObject.Find("RaycastManager"); 
      script = RaycastManager.GetComponent<RaycastManager>(); 
     }
@@ -70,25 +76,42 @@ public class BookManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Wrapper wrapper = new Wrapper();
-        wrapper.List = new List<SaveData>();
-        wrapper = script.Load();
-        for(int i=0; i< wrapper.List.Count; i++ ){
-            for(int j=0; j<39; j++){
-                Debug.Log(wrapper.List[i].food);
-                Debug.Log(item[j].food);
-                if(wrapper.List[i].food == item[j].food){
-                    item[j].times ++;//セーブしたリストの食材名がitem[]の食材名と一致したら入力回数を表すtimes変数を1増やす。
-                    item[j].month = wrapper.List[i].month; //データの月も格納(最後に入力した月に最終的に上書きされる。)
-                    item[j].day = wrapper.List[i].day; //データの月も格納(最後に入力した日に最終的に上書きされる。)
+        if(book_open == true){
+            Wrapper wrapper = new Wrapper();
+            wrapper.List = new List<SaveData>();
+            wrapper = script.Load();
+            for(int i=0; i< wrapper.List.Count; i++ ){
+                for(int j=0; j<39; j++){
+                    if(wrapper.List[i].food == item[j].food){
+                        item[j].times ++;//セーブしたリストの食材名がitem[]の食材名と一致したら入力回数を表すtimes変数を1増やす。
+                        item[j].month = wrapper.List[i].month; //データの月も格納(最後に入力した月に最終的に上書きされる。)
+                        item[j].day = wrapper.List[i].day; //データの月も格納(最後に入力した日に最終的に上書きされる。)
+                    }
+                }
+            }
+            for(int i=0; i<39; i++){
+                if(item[i].times > 0){//もし一回でも入力されていれば
+                    Debug.Log(item[i].food + "を" + item[i].times + "回入力。最後に買った日は" + item[i].month + "月" + item[i].day + "日");
+                    //ボタンを作る。
+                    GameObject cloneObject = Instantiate(button, new Vector3(-2.0f + i * 1.0f, 0.0f, 0.0f), Quaternion.identity);
+                    cloneObject.transform.parent = content.transform;
+                    cloneObject.name = item[i].food;
+                    Text food_text = cloneObject.transform.GetChild(0).GetComponent<Text>();
+                    food_text.text = item[i].food;
                 }
             }
         }
-        for(int i=0; i<39; i++){
-            if(item[i].times > 0){//もし一回でも入力されていれば
-                Debug.Log(item[i].food + "を" + item[i].times + "回入力。最後に買った日は" + item[i].month + "月" + item[i].day + "日");
-            }
-             item[i].times = 0; //Updateで永遠に増えてしまわないように最後に0に戻しておく。
-        }
+        book_open = false;
+    }
+
+    //アイテムボタンクリックイベント内容
+    public void ItemOnClick(GameObject button) {
+         for(int i=0; i<39; i++){
+             if(button.name == item[i].food){
+                text_times.GetComponent<Text>().text = item[i].times + "回";
+                text_lastday.GetComponent<Text>().text = item[i].month + "月" + item[i].day + "日";
+                foodnametext.GetComponent<Text>().text = item[i].food;
+             }
+         }
     }
 }
