7361589c18f7e517e130ef43af63e43594a68b65
Yuka1105 <MpY1105@gmail.com>
2021-12-28 11:48:50 +0900

    ヒストリ削除したモデルに入れ替え

--- Assets/BookManager.cs
+++ Assets/BookManager.cs
@@ -113,7 +113,7 @@ public class BookManager : MonoBehaviour
         if(model){
             Transform modelTransform = model.transform;
             Vector3 localAngle = modelTransform.localEulerAngles;
-            localAngle.y += 1.0f; // ローカル座標を基準に、y軸を軸にし１度分回転
+            localAngle.y += 0.50f; // ローカル座標を基準に、y軸を軸にし１度分回転
             // model.transform.Rotate(new Vector3(0, 1.0f, 0));
             modelTransform.localEulerAngles = localAngle; // 回転角度を設定
         }
