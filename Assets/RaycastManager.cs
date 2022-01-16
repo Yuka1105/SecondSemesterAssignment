1d8580138073b5f8fdb82e77f2d0a9b55b6a3f6e
Yuka1105 <MpY1105@gmail.com>
2022-01-16 13:14:49 +0900

    食べ物を掴んで放す

--- Assets/RaycastManager.cs
+++ Assets/RaycastManager.cs
@@ -22,6 +22,13 @@ public class SaveData //セーブデータ用のクラス
 }
 public class RaycastManager : MonoBehaviour
 {
+    //食べ物を掴んで放す
+    Plane plane;   
+    bool isGrabbing;
+    Transform cube;
+    GameObject movedFood;
+
+
     public bool ate = false;//食べ物を食べたかどうか（CharacterAnimater.csで使う）
     private static RaycastManager instance = null;
 
@@ -52,6 +59,9 @@ public class RaycastManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //食べ物を掴んで放す
+        plane = new Plane(new Vector3(-3f,-4f,13f), new Vector3(0f,0f,13f),new Vector3(1f,2f,13f));
+
         Wrapper wrapper = new Wrapper();
         wrapper.List = new List<SaveData>();
         wrapper = Load();
@@ -121,29 +131,13 @@ public class RaycastManager : MonoBehaviour
                 FoodProvider = GameObject.Find("FoodProvider"); 
                 script = FoodProvider.GetComponent<FoodProvider>();
                 string tag = rayHit.collider.gameObject.tag;//ヒットしたオブジェクトのタグ名を取得
+                movedFood = rayHit.collider.gameObject;
                 if (tag == "red" || tag == "orange" || tag == "yellow" || tag == "green" || tag == "blue"|| tag == "purple" || tag == "pink" || tag == "brown" || tag == "black" || tag == "gray" || tag == "white")//食べ物にヒットした場合のみ
-                {   if(ate == false){//キャラクターの食べるアニメーションが終わる(=false)まで実行しない
-                        ate = true;
-                        TodayNow = DateTime.Now; //時間を取得
-                        SaveData saveData = new SaveData();
-                        saveData.month = TodayNow.Month;
-                        saveData.day = TodayNow.Day;
-                        saveData.food = rayHit.collider.gameObject.name;
-                        int length = saveData.food.Length;//(Clone)という文字を含めた、食べ物の名前の長さを読み取る
-                        saveData.food = saveData.food.Remove(length-7);//lenght-7が食べ物の名前の長さ。末尾の(Clone)の文字を消して再代入
-                        saveData.color = tag;
-                        for(int i = 0; i < script.script.ObjCount; i++){
-                            if(rayHit.collider.gameObject == script.food[i].insta_obj){
-                                saveData.price = script.food[i].food_price;
-                            }
-                        }
-                        Wrapper wrapper = new Wrapper();
-                        wrapper.List = new List<SaveData>();
-                        wrapper = Load();
-                        wrapper.List.Add(saveData);
-                        Save(wrapper);
-                        Destroy(rayHit.collider.gameObject);//食べ物を消す
-                        script.food_num --;
+                {   
+                    if(ate == false){
+                        //食べ物を掴んで放す
+                        isGrabbing = true;
+                        cube = rayHit.transform;
                     }
                 }
                 if(script.food_num == 0 && ate == false){//食べ物が画面上にない場合、食べている最中ではないのみ、図鑑とカレンダーを押せる
@@ -154,6 +148,49 @@ public class RaycastManager : MonoBehaviour
                         SceneManager.LoadScene("Calendar");
                     }
                 }
+            }
+        }
+        //食べ物を掴んで放す
+        if (isGrabbing)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            float rayDistance;
+            plane.Raycast(ray, out rayDistance);
+            cube.position = ray.GetPoint(rayDistance);
+
+            //食べ物を放したとき
+            if (Input.GetMouseButtonUp(0))
+            {
+                //画面外なら中央に食べ物を戻す
+                if(movedFood.transform.position.x < -4 || movedFood.transform.position.x > 4 || movedFood.transform.position.y > -1.5  || movedFood.transform.position.y < -12.5){
+                    cube.position =  new Vector3(-0f, -10f, 13f);
+                }
+                //口のあたりに食べ物を話した場合、画面上から食べ物を消してセーブする。
+                if(movedFood.transform.position.x < 1.5 && movedFood.transform.position.x > -1.5 && movedFood.transform.position.y > -9  && movedFood.transform.position.y < -7){
+                    ate = true;
+                    TodayNow = DateTime.Now; //時間を取得
+                    //セーブ
+                    SaveData saveData = new SaveData();
+                    saveData.month = TodayNow.Month;
+                    saveData.day = TodayNow.Day;
+                    saveData.food = movedFood.name;
+                    int length = saveData.food.Length;//(Clone)という文字を含めた、食べ物の名前の長さを読み取る
+                    saveData.food = saveData.food.Remove(length-7);//lenght-7が食べ物の名前の長さ。末尾の(Clone)の文字を消して再代入
+                    saveData.color = tag;
+                    for(int i = 0; i < script.script.ObjCount; i++){
+                        if(movedFood == script.food[i].insta_obj){
+                            saveData.price = script.food[i].food_price;
+                        }
+                    }
+                    Wrapper wrapper = new Wrapper();
+                    wrapper.List = new List<SaveData>();
+                    wrapper = Load();
+                    wrapper.List.Add(saveData);
+                    Save(wrapper);
+                    script.food_num --;
+                    Destroy(movedFood);//食べ物を消す
+                }
+                isGrabbing = false;
             }
         }
     }
